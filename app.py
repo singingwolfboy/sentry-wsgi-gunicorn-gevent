90c4e2592611f8c6a5451f80308d30e2de958d63
David Baumgold <david@davidbaumgold.com>
2020-03-16 17:35:50 +0100

    Send a message to Sentry

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from sentry_sdk import capture_message
+
 def wsgi_app(environ, start_response):
     data = b"Hello, World!\n"
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
     ])
+    capture_message("This message was sent from WSGI, via Gunicorn/Gevent")
     return iter([data])
